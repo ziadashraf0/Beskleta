5c5e5df7b3028625fdc7b91f1e13f125a6cb7071
ziadashraf0 <ziadshreet@gmail.com>
2020-02-17 18:09:05 +0200

    mergeupdate

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import Login from "./screens/login";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
-import Navigator from "./routes/loginStack";
-
+//import Navigator from "./routes/loginStack";
+import Navigator from "./routes/ClientOwnerStack";
+//import Navigator from "./routes/test";
+import DependentConfirmation from "./screens/dependentConfirmation";
+import Home from "./screens/home";
+import Test from "./screens/testmap";
 export default function App() {
   const getFonts = () => {
     return Font.loadAsync({
@@ -17,7 +21,9 @@ export default function App() {
     return <Navigator></Navigator>;
   } else {
     return (
-      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)}>
+        {" "}
+      </AppLoading>
     );
   }
 }
