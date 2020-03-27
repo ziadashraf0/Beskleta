9afbe44ddab1fe5d8cfd6334ccf7815c9a7819b6
ziadashraf0 <ziadshreet@gmail.com>
2020-03-27 19:20:44 +0200

    notification

---
+++ screens/signOut.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Navigation from "../routes/ClientOwnerStack";
+
+class SignOut extends Component {
+  render() {
+    return <Navigation></Navigation>;
+  }
+}
+export default SignOut;
